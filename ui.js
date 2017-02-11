4a8a39faae76e85c1138072174907eb0adac5f94
White Bread <thewhitebreadman@gmail.com>
2017-02-11 16:29:32 -0500

    Added SmartDashboard values for the robot...

--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ var ui = {
 	timer: document.getElementById('timer'),
 	robotState: document.getElementById('robot-state'),
 	statusCode: document.getElementById('code'),
+	robotSpeed: document.getElementById('motor-speed')
 	gyro: {
 		container: document.getElementById('gyro'),
 		val: 0,
@@ -11,6 +12,21 @@ var ui = {
 		arm: document.getElementById('gyro-arm'),
 		number: document.getElementById('gyro-number')
 	},
+	motorGear: {
+	    gearG: document.getElementById('gear-shift'),
+	    lowG:  document.getElementById('gear-lowG'),
+	    highG: document.getElementById('gear-highG')
+	},
+	/*
+	motorRpm: {
+        left1:  document.getElementById('left-one'),
+        left2:  document.getElementById('left-two'),
+        left3:  document.getElementById('left-three'),
+        right1: document.getElementById('right-one'),
+        right2: document.getElementById('right-two'),
+        right3: document.getElementById('right-three')
+	},
+	*/
 	robotDiagram: {
 		arm: document.getElementById('robot-arm')
 	},
@@ -39,7 +55,8 @@ NetworkTables.addGlobalListener(onValueChanged, true);
 
 
 function onRobotConnection(connected) {
-	var state = connected ? 'Connected' : 'Disconnected';
+	var state = connected ? 'Connected' : 'Dis
+	connected';
 	console.log(state);
 	ui.robotState.innerHTML = state;
 }
@@ -57,9 +74,13 @@ function onValueChanged(key, value, isNew) {
 		case '/SmartDashboard/drive/navx/yaw': // Gyro rotation
 			ui.gyro.val = value;
 			ui.gyro.visualVal = Math.floor(ui.gyro.val - ui.gyro.offset);
-			if (ui.gyro.visualVal < 0) { // Corrects for negative values
+
+			// Corrects for negative values
+			while (ui.gyro.visualVal < 0)
 				ui.gyro.visualVal += 360;
-			}
+			while (ui.gyro.visualVal > 360)
+			    ui.gyro.visualVal -= 360;
+
 			ui.gyro.arm.style.transform = ('rotate(' + ui.gyro.visualVal + 'deg)');
 			ui.gyro.number.innerHTML = ui.gyro.visualVal + 'º';
 			break;
@@ -67,11 +88,12 @@ function onValueChanged(key, value, isNew) {
 			// Info on the actual robot that this works with can be seen at thebluealliance.com/team/1418/2016.
 		case '/SmartDashboard/arm/encoder':
 			// 0 is all the way back, 1200 is 45 degrees forward. We don't want it going past that.
-			if (value > 1140) {
-				value = 1140;
-			} else if (value < 0) {
+
+			if (value > 1140)
+                value = 1140;
+            else if (value < 0)
 				value = 0;
-			}
+
 			// Calculate visual rotation of arm
 			var armAngle = value * 3 / 20 - 45;
 
@@ -90,6 +112,23 @@ function onValueChanged(key, value, isNew) {
 				ui.example.readout.innerHTML = 'Value is false';
 			}
 			break;
+		case '/SmartDashboard/Low gear':
+		    if (value)
+                ui.motorGear.lowG = value;
+            break;
+        case '/SmartDashboard/High gear':
+            if (value)
+              ui.motorGear.highG = value;
+            break;
+        case '/SmartDashboard/Gear'
+            if (value)
+                ui.motorGear.gearG = value;
+            break;
+        case '/SmartDashboard/Speed'
+            if (value)
+                ui.robotSpeed = value;
+            break;
+
 		case '/SmartDashboard/time_running':
 			// When this NetworkTables variable is true, the timer will start.
 			// You shouldn't need to touch this code, but it's documented anyway in case you do.
