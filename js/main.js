8923cb22189cd460d347552a0bceba7efed48390
Andriyasdf <andriymassimilla@gmail.com>
2017-02-14 20:38:17 -0500

    Remove comments and add chart.js

--- js/main.js
+++ js/main.js
@@ -6,60 +6,37 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
-
-// Define global reference to the python server (which we'll start next).
 let server;
 
 function createWindow() {
-    // Start python server.
     if (process.platform === 'win32') {
-        // If on Windows, use the batch command (py -3 ./server.py).
         server = require('child_process').spawn('py', ['-3', '-m', 'pynetworktables2js']);
     } else {
-        // If on unix-like/other OSes, use bash command (python3 ./server.py).
         server = require('child_process').spawn('python3', ['-m', 'pynetworktables2js']);
     }
 
-	// Create the browser window.
 	mainWindow = new BrowserWindow({
         x: 0,
         y: 0,
         width: 1366,
         height: 530,
-        title: "Dashboard - Eagle Imperium",
         icon: "images/logo.ico",
-        resizable: false,
-        frame: false,
         show: false
 	});
 
-	// Move window to top (left) of screen.
 	mainWindow.setPosition(0, 0);
-
-	// Load window.
 	mainWindow.loadURL('http://localhost:8888');
 
-	// Once the python server is ready, load window contents.
-    // TODO: fix this abnomination
 	mainWindow.once('ready-to-show', function() {
 		mainWindow.loadURL('http://localhost:8888');
 		mainWindow.once('ready-to-show', function() {
-			// Once it has reloaded, show the window
 			mainWindow.show();
 		});
 	});
 
-    // Remove menu
     mainWindow.setMenu(null);
-
-	// Emitted when the window is closed.
 	mainWindow.on('closed', function() {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
 		mainWindow = null;
 	});
 }
@@ -70,14 +47,6 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q.
-    // Not like we're creating a consumer application though.
-    // Let's just kill it anyway.
-
-    // If you want to restore the standard behavior, uncomment the next line.
-    // if (process.platform !== 'darwin')
-
     app.quit();
 });
 
