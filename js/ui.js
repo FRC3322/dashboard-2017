402206844a7f37bd99d267b7243d5b99ae5f5251
Andriyasdf <andriymassimilla@gmail.com>
2017-02-19 16:29:16 -0500

    General style improvements

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,8 @@
 NetworkTables.addRobotConnectionListener(onRobotConnection, true);
 NetworkTables.addGlobalListener(onValueChanged, true);
 
-var nt = {};
+var time = [];
+var speed = [];
 
 function onRobotConnection(connected) {
 	var state = connected ? 'Connected' : 'Disconnected';
@@ -23,7 +24,7 @@ function onValueChanged(key, value, isNew) {
 	}
 
 	// Log value changes
-	console.log(key + " = " + value);
+	console.log(key + " | " + value);
 
 	switch (key) {
 		case '/SmartDashboard/time_running':
@@ -54,12 +55,59 @@ function onValueChanged(key, value, isNew) {
 			}
 			NetworkTables.setValue(key, false);
 			break;
+		case '/SmartDashboard/LeftEnc':
+			speed.push(value);
+			break;
+		case '/SmartDashboard/Holder':
+			if (value) {
+				$('#holder').addClass('on');
+			} else {
+				$('#holder').removeClass('on');
+			}
+			break;
+		case '/SmartDashboard/StartPos':
+			if (value == 0) {
+				$('#auton-ready').addClass('on');
+			} else {
+				$('#auton-ready').removeClass('on');
+			}
+			break;
 	}
-
-	// Update a table that reflects
-	nt[key] = value;
 }
 
-$('#network-table').DataTable({
-	"data": nt
+var speedChart = new Chart($("#speed"), {
+	type: 'line',
+	data: {
+		labels: time,
+		datasets: [{
+			label: 'Speed',
+			data: speed,
+			backgroundColor: [
+				'rgba(255, 99, 132, 0.2)',
+				'rgba(54, 162, 235, 0.2)',
+				'rgba(255, 206, 86, 0.2)',
+				'rgba(75, 192, 192, 0.2)',
+				'rgba(153, 102, 255, 0.2)',
+				'rgba(255, 159, 64, 0.2)'
+			],
+			borderColor: [
+				'rgba(255,99,132,1)',
+				'rgba(54, 162, 235, 1)',
+				'rgba(255, 206, 86, 1)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(153, 102, 255, 1)',
+				'rgba(255, 159, 64, 1)'
+			],
+			borderWidth: 1
+		}]
+	},
+	options: {
+		scales: {
+			yAxes: [{
+				ticks: {
+					beginAtZero:true
+				}
+			}]
+		}
+	}
 });
