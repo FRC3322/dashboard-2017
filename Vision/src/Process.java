751adc15fea5f456e3c4966636472087ef08cd26
Andriy Massimilla <andriymassimilla@gmail.com>
2017-10-16 21:24:03 -0400

    Add vision code

---
+++ Vision/src/Process.java
@@ -0,0 +1,35 @@
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+/**
+ * Created by Miles Aikens on 2/16/2017.
+ */
+public class Process {
+    public static VideoCapture videoCapture;
+    public static void main(String[] args){
+        try {
+            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//				opens up the camera stream and tries to load it
+            videoCapture = new VideoCapture();
+            videoCapture.open("http://10.33.22.7:5810/?action=stream");
+            while(!videoCapture.isOpened()){
+                System.out.println("Didn't open Camera, restart jar");
+            }
+            Mat img = new Mat();
+            NetworkTable.setClientMode();
+            NetworkTable.setIPAddress("10.33.22.2");
+            NetworkTable sd = NetworkTable.getTable("SmartDashboard");
+            EagleVision eagle = new EagleVision(sd);
+            while(true){
+                videoCapture.read(img);
+                eagle.process(img);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
